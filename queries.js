7ca61079f9a27a7f9cec3345acfc990b9e8b5634
Josue Valencia <josue.valencia@domo.ec>
2019-03-23 15:00:40 +0100

    companies

--- queries.js
+++ queries.js
@@ -23,3 +23,25 @@
 	db.restaurants.updateOne({	name: 'Morris Park Bake Shop', }, {$set:{address: 'Calle falsa 123'}})  //1 modified
 // Delete all the restaurants with address zipcode 10466.
 	db.restaurants.deleteMany({“address.zipcode”:"10466"}) //delete 15
+
+
+// Companies
+//Find all the companies that include 'Facebook' on the name field.
+db.companies.find({name:{$regex:'.*Facebook*.'}}, {name:1, _id:0})
+
+// Find all the companies which category_code is 'web'. Retrive only their name field:
+db.companies.find({category_code:'web'}, {name:1, _id:0})
+// Find all the companies named "Twitter", and retrieve only their name, category_code and founded_year fields.
+db.companies.find({name:'Twitter'}, {name:1,category_code:1, founded_year:1, _id:0}) //named Twitter
+db.companies.find({name:{$regex:'.*Twitter*.'}}, {name:1,category_code:1, founded_year:1, _id:0}) //Includes twitter
+// Find all the companies who have web as their category_code, but limit the search to 50 companies.
+db.companies.find({category_code:{$regex:'.*web*.'}}, {name:1, _id:0, category_code: 1}).limit(50)
+
+// Find all the companies which category_code is 'enterprise' and have been founded in 2005. Retrieve only the name, category_code and founded_year fields.
+db.companies.find({category_code:{$regex:'.*enterprise*.'}, founded_year:2005}, {name:1, _id:0, category_code: 1, founded_year:1})
+// Find all the companies that have been founded on the 2000 or have 20 employees. Sort them descendingly by their number_of_employees.
+
+db.companies.find({$or:[founded_year:2000, employees: 20]}, {name:1, category_code:1, }).sort({employees:-1})
+
+
+
