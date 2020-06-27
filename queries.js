f39dd0420a2223d3e92cb50479d5c265b627f532
Irma Blanco <irblancofdz@gmail.com>
2020-06-27 14:17:13 +0200

    Iteration 3 in progress

--- queries.js
+++ queries.js
@@ -70,3 +70,36 @@ db.restaurants.updateOne({
 db.restaurants.deleteMany({
   'address.zipcode': '10466'
 })
+
+// Find all the companies that include 'Facebook' on the name field.
+db.companies.find({
+  name: 'Facebook'
+}).pretty()
+// Find all the companies which category_code is 'web'. Retrive only their name field:
+db.companies.find({
+  category_code: 'web'
+}, {
+  name: 1,
+  _id: 0
+}).pretty()
+// Find all the companies named "Twitter", and retrieve only their name, category_code and founded_year fields.
+db.companies.find({
+  name: 'Twitter'
+}, {
+  name: 1,
+  category_code: 1,
+  founded_year: 1,
+  _id: 0
+}).pretty()
+// Find all the companies who have web as their category_code, but limit the search to 50 companies.
+db.companies.find({
+  category_code: 'web'
+}).limit(50)
+// Find all the companies which category_code is 'enterprise' and have been founded in 2005. Retrieve only the name, category_code and founded_year fields.
+// Find all the companies that have been founded on the 2000 or have 20 employees. Sort them descendingly by their number_of_employees.
+// Find all the companies that do not include web nor social on their category_code. Limit the search to 20 documents and retrieve only their name and category_code.
+// Find all the companies that were not founded on 'June'. Skip the first 50 results and retrieve only the founded_month and name fields.
+// Find all the companies that have 50 employees, but do not correspond to the 'web' category_code.
+// Find all the companies that have been founded on the 1st of the month, but does not have either 50 employees nor 'web' as their category_code. Retrieve only the founded_day and name and limit the search to 5 documents.
+// Find all the companies which the price_amount of the acquisition was 40.000.000. Sort them by name.
+// Find all the companies that have been acquired on January of 2014. Retrieve only the acquisition and name fields.
