90dabae94e85aa4bad81beef4a83677ac87bcd3e
Nidian Martin <nidianmartin@gmail.com>
2020-06-28 22:57:01 +0200

    exercise finished

--- queries.js
+++ queries.js
@@ -33,6 +33,19 @@ db.companies.find({"name": "Twitter"}, {"name": 1, "category_code": 1, "founded_
 //4 Find all the companies who have web as their category_code, but limit the search to 50 companies.
 db.companies.find({"category_code": "web"}).limit(50).pretty() 
 //Busque la función limit no dio error asi que confio en que saco 50.
-
+//5.Find all the companies which category_code is ‘enterprise’ and have been founded in 2005. Retrieve only the name, category_code and founded_year fields.
+db.companies.find({"category_code": {$eq:"enterprise"},"founded_year":{$eq: 2005}},{"name": 1, "category_code": 1, "founded_year":1, _id: 0}).pretty()
+//6.Find all the companies that have been founded on the 2000 or have 20 employees. Sort them descendingly by their number_of_employees.
+db.companies.find({ $or: [ {"founded_year":{$eq:2000}}, {"number_of_employees": {$eq: 20}}]}).sort({ "number_of_employees": -1}).pretty()
+//7.Find all the companies that do not include web nor social on their category_code. Limit the search to 20 documents and retrieve only their name and category_code.
+db.companies.find({ $or: [ {"category_code": { $ne: "web" }}, { "category_code": { $ne:"social" }} ] }, { "name":1, "category_code":1, _id:0 }).limit(20).pretty()
+//8.Find all the companies that were not founded on ‘June’. Skip the first 50 results and retrieve only the founded_month and name field
+db.companies.find({ "founded_month": {$ne:6} },{ "founded_month":1, "name":1, "_id":0 }).skip(50).pretty()
+//9.Find all the companies that have 50 employees, but do not correspond to the ‘web’ category_code.
+db.companies.find({ "number_of_employees": {$eq: 50}, "category_code": {$ne: "web"} }).pretty()
+//10.Find all the companies that have been founded on the 1st of the month, but does not have either 50 employees nor ‘web’ as their category_code. Retrieve only the founded_day and name and limit the search to 5 documents.
+db.companies.find({ "founded_day": {$eq: 1}, "number_of_employees": {$ne: 50}, "category_code": {$ne:"web"}}, {"found_day":1, "name":1, "_id":0}).limit(5).pretty()
+//11.Find all the companies which the price_amount of the acquisition was 40.000.000. Sort them by name.
+db.companies.find({ "acquisition.price_amount":{$eq:40000000}}).sort({"name": 1}).pretty()
 //12 Find all the companies that have been acquired on January of 2014. Retrieve only the acquisition and name fields.
 db.companies.find({"acquisition.acquired_year": 2014, "acquisition.acquired_month": 01}, {"name": 1, "acquisition": 1}).pretty()
