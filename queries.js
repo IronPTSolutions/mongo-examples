7e44576025ca9413bd2ea8d5264170a18e92f1f7
gonzalomerino95 <gonmerino95@gmail.com>
2021-06-15 20:44:24 +0200

    Lab acabado

--- queries.js
+++ queries.js
@@ -19,4 +19,21 @@
 //5. db.restaurants.find({ "grades.score": { $gt: 90 }}).pretty()
 //6. db.restaurants.find({ "cuisine": {$ne: "Bakery"}, "grades.score": { $gt: 70 }}).pretty()
 //7. db.restaurants.find({ "cuisine": {$ne: "Chinese"}, "grades.grade": "A", "borough": {$ne: "Manhattan"}}).pretty()
-//8. db.restaurants.updateMany({ "cuisine": "American " }, { $set: { "cuisine": "American" } });
+//8. db.restaurants.updateMany({ "cuisine": "American " }, { $set: { "cuisine": "American" } })
+//9. db.restaurants.updateOne({ "name": "Morris Park Bake Shop" }, { $set: { "address.street": "Calle falsa 123"} })
+//10. db.restaurants.deleteMany({ "address.zipcode": "10466" })
+
+//Companies
+//1. db.companies.find({"name": "Facebook"}).pretty()
+//2. db.companies.find({"category_code": "web"},{name:1}).pretty()
+//3. db.companies.find({"name": "Twitter"},{name:1, category_code:1, founded_year:1}).pretty()
+//4. db.companies.find({"category_code": "web"}).limit(50).pretty()
+//5. db.companies.find($and:[{"category_code": "enterprise"}, {"founded_year": "2005"}],{name:1, category_code:1, founded_year:1}).pretty()
+//6. db.companies.find( {category_code: "enterprise",founded_year:2005}, {_id: 0, name: 1, category_code: 1, employees: 1}).pretty()
+//7. db.collection.find({ $or:[ {founded_year: 2000}, {number_of_employees: 20} ] }).sort( { number_of_employees: -1 } ).pretty()
+//8. db.companies.find({category_code:{$ne:"web"},category_code:{$ne:"social"}},{_id: 0,name:1,category_code:1}).limit(20).pretty()
+//9. db.companies.find({founded_month:{$ne:6}},{_id:0,name:1,founded_month:1}).skip(50).pretty()
+//10. db.companies.find({category_code: {$ne: "web"}, number_of_employees:50}).pretty()
+//11. db.companies.find({founded_day: 1, $nor:[{number_of_employees: 50}, {category_code: "web"}]}, {founded_day:1, name:1}).limit(5).pretty()
+//12. db.companies.find({"acquisition.price_amount":40000000}).sort({"name":1}).pretty()
+//13. db.companies.find({"acquisition.acquired_year": 2014, "acquisition.acquired_month": 1}, {_id: 0, name: 1, acquisition: 1}).pretty()
