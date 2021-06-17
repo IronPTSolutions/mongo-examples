27d1f23bfaa7c6854b1b500bee15b9d7257c5d1b
Pablo Borjas <pabloborjas_0708@hotmail.com>
2021-06-17 11:25:22 +0200

    terminado

--- queries.js
+++ queries.js
@@ -17,7 +17,7 @@ db.employees.updateOne({name:"Willy"},{$set:{"phone.personal": 93-123-45-65}})
 //employees 9
 db.employees.updateOne({name:"Bob"},{$set:{privileges:"user"}})
 //employees 10
-db.employees.find({"favorites.artist": "Picasso"}).pretty()
+db.employees.find({"favorites.artist": "Picasso"})
 //employyes 11
 db.employees.deleteOne({_id: ObjectId("60c475b64fa53a6877b8e03a")})
 
@@ -45,27 +45,27 @@ db.restaurants.updateOne({name: "Morris Park Bake Shop"},{$set: {"address.street
 //restaurants 11
 db.restaurants.deleteMany({"address.zipcode": 10466})
  
-
-//companies 1
-
-//companies 2
-
-//companies 3
-
-//companies 4
-
-//companies 5
-
-//companies 6
-
-//companies 7
-
-//companies 8
-
-//companies 9
-
-//companies 10
-
-//companies 11
-
-//companies 12
+//Companies 1
+db.companies.find({name:"Facebook"})
+//Companies 2
+db.companies.find({category_code:"web"},{name:1})
+//Companies 3
+db.companies.find({name:"Twitter"},{name:1, category_code:1, founded_year:1})
+//Companies 4
+db.companies.find({category_code:"web"}).limit(50)
+//Companies 5
+db.companies.find({$and:[ {category_code:"enterprise"}, {founded_year:2005}]}, {name:1, category_code:1, founded_year:1})
+//Companies 6
+db.companies.find({$or:[ {founded_year:2000}, {number_of_employees:20}]}).sort({number_of_employees:-1})
+//Companies 7
+db.companies.find({$nor:[ {category_code:"web"}, {category_code:"social"}]}, {name:1, category_code:1}).limit(20)
+//Companies 8
+db.companies.find({founded_month:{$ne:6}},{name:1, founded_month:1}).skip(50)
+//Companies 9
+db.companies.find({$and:[ {number_of_employees:50, category_code:{$ne:"web"}}]})
+//Companies 10
+db.companies.find({$and:[ {founded_month:1, $nor:[ {category_code:"web"}, {number_of_employees:50}]}]}, {name:1, founded_day:1}).limit(5)
+//Companies 11
+db.companies.find({"acquisitions.price_amount":40000000}).sort({name:1})
+//Companies 12
+db.companies.find({$and:[ {"acquisition.acquired_year":2014, "acquisition.acquired_month":1}]},{name:1, acquisition:1})
