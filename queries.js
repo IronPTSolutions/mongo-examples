77eec84e113c27a144074857f37876c12169518d
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-01-20 12:59:56 +0100

    finish iteration 2

--- queries.js
+++ queries.js
@@ -37,14 +37,25 @@ db.restaurants.find().pretty()
 //Find all the restaurants and display only the fields restaurant_id, name, borough and cuisine.
 db.restaurants.find({}, { "_id": 1, "name": 1, "borough": 1, "cuisine": 1 } ).pretty()
 //Find all the restaurants and display only the fields restaurant_id, name, borough and zip code.
+db.restaurants.find({}, { "_id": 1, "name": 1, "borough": 1, "address.zipcode": 1 } ).pretty()
 //Find the restaurants which are in the borough Bronx.
+db.restaurants.find({ "borough": "Bronx" }).pretty()
 //Find the restaurants which are in the borough Brooklyn with Steak cuisine.
+db.restaurants.find({ "borough": "Brooklyn", "cuisine": "Steak" }).pretty()
 //Find the restaurants which have achieved a score bigger than 90.
+db.restaurants.find({ "grades.score": { $gt: 90 } }).pretty()
 //Find the restaurants that do not prepare any Bakery cuisine and with a grade score equal or bigger than 70.
+db.restaurants.find({ "cuisine": { $ne: "Bakery" } ,"grades.score": { $gte: 70 } }).pretty()
 //Find the restaurants which do not prepare any Chinese cuisine and have achieved a grade point A which do not belong to the borough Manhattan.
+db.restaurants.find({ "cuisine": { $ne: "Chinese" }, "grades.grade": { $eq: "A" }, "borough": { $ne: "Manhattan" } }).pretty()
 //Update restaurants with 'American ' cuisine to 'American' (without the space!!!)
+db.restaurants.updateMany({ "cuisine": "American "}, { $set: { "cuisine": "American" } })
 //Update Morris Park Bake Shop address street to Calle falsa 123.
+db.restaurants.updateOne({ "name": "Morris Park Bake Shop"}, { $set: { "address.street": "Calle falsa 123" } })
 //Delete all the restaurants with address zipcode 10466.
+db.restaurants.deleteMany({ "address.zipcode": "10466" })
+
+
 
 
 
