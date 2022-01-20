26e5238a7fdd026e76bf391842c3016c296a7ad5
Maria Gallego <mariagestionredes@gmail.com>
2022-01-20 11:43:28 +0100

    done restaurants

--- queries.js
+++ queries.js
@@ -6,7 +6,9 @@ print('Employees')
 print('1. List all Employees')
 db.employees.find({}).forEach(printjsononeline)
 
-/* List all the employees */
+/* EMPLOYEES
+
+List all the employees */
 
 db.employees.find()
 
@@ -49,3 +51,50 @@ db.employees.find( { "favorites.artist" : { $eq : "Picasso" } })
 /* Delete the user John */
 
 db.employees.deleteOne( { name : "John"} )
+
+/* RESTAURANTS
+
+List all the restaurants */
+
+db.restaurants.find()
+
+/* Find all the restaurants and display only the fields restaurant_id, name, borough and cuisine */
+
+db.restaurants.find( {}, {"restaurant_id" : 1, "name": 1, "borough": 1, "cuisine": 1, "_id": 0})
+
+/* Find all the restaurants and display only the fields restaurant_id, name, borough and zip code */
+
+db.restaurants.find( {}, {"restaurant_id" : 1, "name": 1, "borough": 1, "address.zipcode": 1, "_id": 0})
+
+/* Find the restaurants which are in the borough Bronx */
+
+db.restaurants.find( { borough : "Bronx"} )
+
+/* Find the restaurants which are in the borough Brooklyn with Steak cuisine */
+
+db.restaurants.find( { $and: [ { borough: "Brooklyn"}, { cuisine: "Steak"} ] })
+
+/* Find the restaurants which have achieved a score bigger than 90 */
+
+db.restaurants.find( { "grades.score" : { $gt: 90} })
+
+
+/* Find the restaurants that do not prepare any Bakery cuisine and with a grade score equal or bigger than 70 */
+
+db.restaurants.find( { $and : [ { "grades.score" : { $gte : 70}}, { cuisine : { $ne : "bakery" } } ] })
+
+/* Find the restaurants which do not prepare any Chinese cuisine and have achieved a grade point A which do not belong to the borough Manhattan */
+
+db.restaurants.find( { $and : [ { $nor : [ { cuisine : "Chinese"}, { borough : "Manhattan"} ] }, { "grades.grade" : { $eq : "A"} } ] } )
+
+/* Update restaurants with 'American ' cuisine to 'American' (without the space!!!) */
+
+db.restaurants.updateMany( { cuisine : "American " }, { $set : { cuisine : "American" } } )
+
+/* Update Morris Park Bake Shop address street to Calle falsa 123 */
+
+db.restaurants.updateOne( { name : "Morris Park Bake Shop" }, { $set : { " address.street" : "Calle falsa 123" } } )
+
+/* Delete all the restaurants with address zipcode 10466 */
+
+db.restaurants.deleteMany( { "address.zipcode" : "10466" } )
