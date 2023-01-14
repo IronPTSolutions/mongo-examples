444a01ac53ae289efb451d923cfd1fac2517c241
patri <patricia.mareg@gmail.com>
2023-01-14 14:12:36 +0100

    not finished but what i have

--- queries.js
+++ queries.js
@@ -39,3 +39,37 @@ db.employees.find({"favorites.artist": "Picasso"}).pretty()
 
 // Delete the user John.
 db.employees.deleteOne({name: "John"})
+
+
+// List all the restaurants.
+db.restaurants.find()
+
+// Find all the restaurants and display only the fields restaurant_id, name, borough and cuisine.
+db.restaurants.find({}, {restaurant_id: 1, name: 1, borough: 1, cuisine: 1}).pretty()
+
+// Find all the restaurants and display only the fields restaurant_id, name, borough and zip code.
+db.restaurants.find({}, {restaurant_id: 1, name: 1, borough: 1, "address.zipcode": 1}).pretty()
+
+// Find the restaurants which are in the borough Bronx.
+db.restaurants.find({borough: "Bronx"}).pretty()
+
+// Find the restaurants which are in the borough Brooklyn with Steak cuisine.
+db.restaurants.find({$and: [{borough: "Brooklyn"}, {cuisine: "Steak"}]}).pretty()
+
+// Find the restaurants which have achieved a score bigger than 90.
+db.restaurants.find({"grades.score": {$gt: 90}}).pretty()
+
+// Find the restaurants that do not prepare any Bakery cuisine and with a grade score equal or bigger than 70.
+db.restaurants.find({$and: [{cuisine: {$ne: "Bakery"}}, {"grades.score": {$gte: 70}}]}).pretty()
+
+// Find the restaurants which do not prepare any Chinese cuisine and have achieved a grade point A which do not belong to the borough Manhattan.
+db.restaurants.find({$and: [{cuisine: {$ne: "Chinese"}}, {"grades.grade": "A"}, {borough: {$ne: "Manhattan"}}]}).pretty()
+
+// Update restaurants with 'American ' cuisine to 'American' (without the space!!!)
+db.restaurants.updateMany({cuisine: "American "}, {$set: {cuisine: "American"}})
+
+// Update Morris Park Bake Shop address street to Calle falsa 123.
+db.restaurants.updateOne({name: "Morris Park Bake Shop"}, {$set: {"address.street": "Calle falsa 123"}})
+
+// Delete all the restaurants with address zipcode 10466.
+db.restaurants.deleteOne({"address.zipcode": "10466"})
